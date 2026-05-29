a3e553250e97df3cb33d218e113f63362667a9a6
mattcolborn <matt.colborn@gmail.com>
2026-05-29 06:33:49 +0100

    Add PyCharm and pyenv support

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crater-classifier",
+    version="0.1",
+    description="Binary CNN image classifier for cratered vs non-cratered terrain",
+    author="Matt Colborn",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.11",
+    install_requires=[
+        "torch",
+        "torchvision",
+        "matplotlib",
+        "pillow",
+    ],
+)
